b813468aa72934fe4fd184a41f8d051cfa7b9dc9
Tommy Bishop <bishop.thomas@gmail.com>
2013-02-07 16:26:51 -0800

    fix metadata file

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-name             'aws_alert_monitor-cookbook'
+name             'aws_alert_monitor'
 maintainer       'Intuit, Inc.'
 maintainer_email 'thomas_bishop@intuit.com'
 license          'Apache 2.0'
-description      'Installs/Configures aws_alert_monitor-cookbook'
+description      'Installs/Configures aws_alert_monitor'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
